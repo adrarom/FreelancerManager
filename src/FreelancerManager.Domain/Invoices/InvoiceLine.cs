951d4691d4924fd5306d586b909d7c7b831dcbca
adrarom <adrarom@gmail.com>
2026-08-16 23:51:36 +0200

    feat: add invoice line domain model and tests

---
+++ src/FreelancerManager.Domain/Invoices/InvoiceLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreelancerManager.Domain.Invoices
+{
+    public class InvoiceLine
+    {
+        public string Description { get; }
+        public decimal Quantity { get; }
+
+        public decimal UnitPrice { get; }
+
+        public decimal Subtotal => UnitPrice * Quantity;
+
+        public InvoiceLine(string description, decimal quantity, decimal unitPrice)
+        {
+            Description = description;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+
+    }
+}
---
+++ tests/FreelancerManager.UnitTests/Domain/Invoices/InvoicesLinesTest.cs
@@ -0,0 +1,48 @@
+﻿using FreelancerManager.Domain.Invoices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace FreelancerManager.UnitTests.Domain.Invoices
+{
+    public class InvoicesLinesTest
+    {
+        [Fact]
+        public void Constructor_WithValidValues_CreatesInvoiceLine()
+        {
+            // Arrange
+            var description = "Backend development";
+            var quantity = 10m;
+            var unitPrice = 50m;
+
+            // Act
+
+            var invoiceLine = new InvoiceLine(description, quantity, unitPrice);
+
+            //Assert
+
+            Assert.Equal(description, invoiceLine.Description);
+            Assert.Equal(quantity, invoiceLine.Quantity);
+            Assert.Equal(unitPrice, invoiceLine.UnitPrice);
+        }
+
+        [Fact]
+        public void Subtotal_WithValidValues_ReturnQuantityTimesUnitPrice()
+        {
+            
+            // Arrange
+
+            var description = "Backend development";
+            var quantity = 10m;
+            var unitPrice = 50m;
+
+            // Act
+
+            var invoiceLine = new InvoiceLine(description, quantity, unitPrice);
+
+            //Assert
+
+            Assert.Equal(500m, invoiceLine.Subtotal);
+        }
+    }
+}
