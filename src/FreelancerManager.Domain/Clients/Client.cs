c4ffb763fcfe9f2f47d46cdef9d7966d99839634
Adrián Araque <adrarom@gmail.com>
2026-08-17 22:08:19 +0200

    feat: add client domain model and validations (#5)
    * feat: add client domain model and validations
    
    * fix: enforce client input invariants
    
    * test: correct client invariant test cases

---
+++ src/FreelancerManager.Domain/Clients/Client.cs
@@ -0,0 +1,37 @@
+﻿namespace FreelancerManager.Domain.Clients
+{
+    public class Client
+    {
+        public string Name { get; }
+        public string TaxId { get; }
+        public string? Email { get; }
+
+        public Client(string name, string taxId, string? email)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(
+                    "Name cannot be empty.",
+                    nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(taxId))
+            {
+                throw new ArgumentException(
+                    "TaxId cannot be empty.",
+                    nameof(taxId));
+            }
+
+            if (email is not null && string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentException(
+                    "Email cannot be empty or whitespace.",
+                    nameof(email));
+            }
+
+            Name = name;
+            TaxId = taxId;
+            Email = email;
+        }
+    }
+}
