fc7025142e7ea85cca6971bfb0339c5860d13f41
adrarom <adrarom@gmail.com>
2026-08-17 18:57:51 +0200

    feat: add invoice lifecycle and issuing rules

--- src/FreelancerManager.Domain/Invoices/Invoice.cs
+++ src/FreelancerManager.Domain/Invoices/Invoice.cs
@@ -8,10 +8,25 @@
 
         public decimal Subtotal => _lines.Sum(line => line.Subtotal);
 
+        public InvoiceStatus Status { get; private set; } = InvoiceStatus.Draft;
         public void AddLine(InvoiceLine line)
         {
             if(line is null) throw new ArgumentNullException(nameof(line),"InvoiceLine can't be null");
             _lines.Add(line);
+        }
+
+        public void Issue()
+        {
+            if (_lines.Count <= 0)
+            {
+                throw new InvalidOperationException("Invoice cannot be issued without lines.");
+            }
+
+            if (Status == InvoiceStatus.Issued) 
+            {
+                throw new InvalidOperationException("Invoice status is already Issued");
+            }
+            Status = InvoiceStatus.Issued;
         }
     }
 }
